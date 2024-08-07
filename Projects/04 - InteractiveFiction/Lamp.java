fdeb199bfe3edabeb312234b7c5898e54a1d0f94
Satchit Seth <satchit.seth30@gmail.com>
2024-08-06 20:46:29 -0400

    Update Lamp.java

--- Projects/04 - InteractiveFiction/Lamp.java
+++ Projects/04 - InteractiveFiction/Lamp.java
@@ -1,1 +1,23 @@
+// For an overview of the program and sample inputs, be sure to check out `InteractiveFiction.md`
+
+// Methods and getter/setters for the light status
+public class Lamp {
+    private boolean isLit = false;
+
+    public void turnOn() {
+        isLit = true;
+    }
+
+    public boolean isLit() {
+        return isLit;
+    }
+    
+    public boolean getIsLit() {
+    	return isLit;
+    }
+    
+    public void setIsLit(boolean litStatus) {
+    	isLit = litStatus;
+    }
+}
 
