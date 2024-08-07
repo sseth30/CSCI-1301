2df5f909007964af0fb2479bc2e005e233af6a5b
Satchit Seth <satchit.seth30@gmail.com>
2024-08-06 20:47:09 -0400

    Update Player.java

--- Projects/04 - InteractiveFiction/Player.java
+++ Projects/04 - InteractiveFiction/Player.java
@@ -1,1 +1,54 @@
+// For an overview of the program and sample inputs, be sure to check out `InteractiveFiction.md`
+
+public class Player {
+    private int x;
+    private int y;
+    private Lamp lamp;
+    private Key key;
+
+    // No-argument constructor
+    public Player() {
+        this(0, 0);  
+    }
+
+    // Constructor with parameters
+    public Player(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+        this.lamp = null;
+        this.key = null;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void setLamp(Lamp newLamp) {
+        lamp = newLamp;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key newKey) {
+        key = newKey;
+    }
+}
 
