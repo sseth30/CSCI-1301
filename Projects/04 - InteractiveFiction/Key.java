93799cb3ef4c044425b34da3b3216c124b1dd314
Satchit Seth <satchit.seth30@gmail.com>
2024-08-06 23:34:00 -0400

    Update Key.java

--- Projects/04 - InteractiveFiction/Key.java
+++ Projects/04 - InteractiveFiction/Key.java
@@ -1,8 +1,6 @@
 // For an overview of the program and sample inputs, be sure to check out `InteractiveFiction.md`
+// This class should remain unchanged for full functionality
 
-/*You do not need to modify this class
- * 
- */
 public class Key {
 
 	//This method is called when you are ready to unlock a chest with the key obtained from the map
